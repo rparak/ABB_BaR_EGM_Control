b3e79ddafbc37201307a287f166b8860b54949f1
Roman Parak <59449320+rparak@users.noreply.github.com>
2021-04-28 15:29:52 +0100

    Update Main.c

--- Automation_Studio_BaR/OPCUa_Client_ABB_EGM/Logical/Server_t/Main.c
+++ Automation_Studio_BaR/OPCUa_Client_ABB_EGM/Logical/Server_t/Main.c
@@ -108,10 +108,8 @@ void _CYCLIC ProgramCyclic(void)
 	if(abb_control.Command.joint.start == 1){
 		abb_control.Command.joint.is_started      = 1;
 		abb_control.Command.cartesian.is_started  = 0;
-		abb_control.Command.joint.start_read_data = 0;
 	}else if(abb_control.Command.joint.stop == 1){
-		abb_control.Command.joint.is_started     = 0;
-		abb_control.Command.joint.stop_read_data = 0;
+		abb_control.Command.joint.is_started = 0;
 	}
 	// -------------------- START READ {JOINT} -------------------- //
 	if(abb_control.Command.joint.start_read_data == 1){
@@ -124,10 +122,8 @@ void _CYCLIC ProgramCyclic(void)
 	if(abb_control.Command.cartesian.start == 1){
 		abb_control.Command.cartesian.is_started  = 1;
 		abb_control.Command.joint.is_started      = 0;
-		abb_control.Command.cartesian.start_read_data = 1;
 	}else if(abb_control.Command.cartesian.stop == 1){
-		abb_control.Command.cartesian.is_started     = 0;
-		abb_control.Command.cartesian.stop_read_data = 0;
+		abb_control.Command.cartesian.is_started = 0;
 	}
 	// -------------------- START READ {CARTESIAN} -------------------- //
 	if(abb_control.Command.cartesian.start_read_data == 1){
